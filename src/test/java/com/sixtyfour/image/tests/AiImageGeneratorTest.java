6581938c825e6dd362bdcad88e6ba6663903b395
EgonOlsen <github@jpct.net>
2024-06-03 18:09:50 +0200

    Better OpenAI error handling

--- src/test/java/com/sixtyfour/image/tests/AiImageGeneratorTest.java
+++ src/test/java/com/sixtyfour/image/tests/AiImageGeneratorTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class AiImageGeneratorTest {
 
     public static void main(String[] args) throws Exception {
-        List<String> images = AiImageGenerator.createImages("wet cunts");
+        List<String> images = AiImageGenerator.createImages("a cat on the beach");
         images.forEach(System.out::println);
     }
 
